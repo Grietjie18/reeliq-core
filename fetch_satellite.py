cfa3dfce001509c66f423e42817d60edc1a55c9f
Grietjie18 <mburger549@gmail.com>
2026-03-28 21:21:12 +0200

    Update fetch_satellite.py

--- fetch_satellite.py
+++ fetch_satellite.py
@@ -1,7 +1,7 @@
 import copernicusmarine
 
 if __name__ == "__main__":
-    results = copernicusmarine.describe(contains=["SST", "L4", "NRT", "GLO"])
+    results = copernicusmarine.describe(contains=["OSTIA"])
     for p in results.products:
         print(p.product_id)
         for d in p.datasets:
